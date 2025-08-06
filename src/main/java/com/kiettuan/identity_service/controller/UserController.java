12c168da6d0db40b2e808e3434cfffd5e13ce212
KietTuan <theonlytruth25012005@gmail.com>
2025-08-06 20:43:13 +0700

    basic validation for password

--- src/main/java/com/kiettuan/identity_service/controller/UserController.java
+++ src/main/java/com/kiettuan/identity_service/controller/UserController.java
@@ -4,6 +4,7 @@ import com.kiettuan.identity_service.dto.request.UserCreationRequest;
 import com.kiettuan.identity_service.dto.request.UserUpdateRequest;
 import com.kiettuan.identity_service.entity.User;
 import com.kiettuan.identity_service.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +16,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    User createUser(@RequestBody UserCreationRequest request){
+    User createUser(@RequestBody @Valid UserCreationRequest request){
         return userService.createUser(request);
     }
 
