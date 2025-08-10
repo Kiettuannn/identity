a3e13229533f8b63fd61bd6570a19ff0d1a8cfdb
KietTuan <theonlytruth25012005@gmail.com>
2025-08-10 15:20:50 +0700

    Authorization: User must have the token to get their information

--- src/main/java/com/kiettuan/identity_service/service/UserService.java
+++ src/main/java/com/kiettuan/identity_service/service/UserService.java
@@ -12,7 +12,10 @@ import com.kiettuan.identity_service.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,6 +26,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
 public class UserService {
     UserRepository userRepository;
     PasswordEncoder passwordEncoder;
@@ -42,11 +46,15 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     public List<User> getUsers(){
+        log.info("In method get Users");
         return userRepository.findAll();
     }
 
+    @PostAuthorize("returnObject.username == authentication.name")
     public UserResponse getUser(String id){
+        log.info("In method gets a user by Id");
         return  userMapper.toUserResponse(userRepository.findById(id)
                 .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND)));
     }
