37c34638cdfb685f71fe44097e7c4bddec15f377
KietTuan <theonlytruth25012005@gmail.com>
2025-08-14 10:27:24 +0700

    Step 2: Create InvalidatedToken entity

---
+++ src/main/java/com/kiettuan/identity_service/entity/InvalidatedToken.java
@@ -0,0 +1,22 @@
+package com.kiettuan.identity_service.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class InvalidatedToken {
+    @Id
+    String id;
+    Date expiryTime;
+}
