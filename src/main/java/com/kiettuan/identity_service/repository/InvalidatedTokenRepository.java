f42ef910e2c21a0ba6b4d96e90ef6ff440a4b312
KietTuan <theonlytruth25012005@gmail.com>
2025-08-14 17:15:18 +0700

    create cron-job to delete all expired tokens

--- src/main/java/com/kiettuan/identity_service/repository/InvalidatedTokenRepository.java
+++ src/main/java/com/kiettuan/identity_service/repository/InvalidatedTokenRepository.java
@@ -1,7 +1,19 @@
 package com.kiettuan.identity_service.repository;
 
 import com.kiettuan.identity_service.entity.InvalidatedToken;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Repository
 public interface InvalidatedTokenRepository extends JpaRepository<InvalidatedToken,String> {
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM InvalidatedToken it WHERE it.expiryTime < :now")
+    void deleteAllExpiredTokens(Date now);
 }
