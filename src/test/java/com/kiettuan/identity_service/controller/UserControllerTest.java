802d9486aca7113134de4b4a395dcab96f325850
KietTuan <theonlytruth25012005@gmail.com>
2025-08-15 12:16:00 +0700

    add Dependencies jackson to use LocalDate, Refractor UserService and Controller: User -> UserResponse; Mapper... Add log in controller and service, add register to use jackson - LocalDate, add WHEN, THEN (andExpect)

--- src/test/java/com/kiettuan/identity_service/controller/UserControllerTest.java
+++ src/test/java/com/kiettuan/identity_service/controller/UserControllerTest.java
@@ -2,12 +2,15 @@ package com.kiettuan.identity_service.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.kiettuan.identity_service.dto.request.UserCreationRequest;
 import com.kiettuan.identity_service.dto.response.UserResponse;
 import com.kiettuan.identity_service.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +22,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
@@ -57,20 +61,26 @@ public class UserControllerTest {
     }
 
     @Test
-    void createUser() throws Exception {
+    // Test with correct data
+    void createUser_validRequest_success() throws Exception {
         // GIVEN
-        ObjectMapper objectmapper = new ObjectMapper(); // convert object to string
-        String content = objectmapper.writeValueAsString(request);
+        ObjectMapper objectmapper = new ObjectMapper();
+        objectmapper.registerModule(new JavaTimeModule()); // register for `jackson` package identify LocalDate type
 
-        // WHEN
+        String content = objectmapper.writeValueAsString(request); // convert object to string
+
+        // Mock method call service filter
+        Mockito.when(userService.createUser(ArgumentMatchers.any()))
+                .thenReturn(userResponse);
+
+        // WHEN, THEN
         mockMvc.perform(MockMvcRequestBuilders
-                    .post("/users")
-                    .contentType(MediaType.APPLICATION_JSON_VALUE)
-                    .content(content))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("code").value(200));
+                    .post("/users") // method
+                    .contentType(MediaType.APPLICATION_JSON_VALUE) // data type
+                    .content(content)) // response
+                .andExpect(MockMvcResultMatchers.status().isOk()) // status
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value(1000)); // code
 
-        // THEN
 
     }
 
