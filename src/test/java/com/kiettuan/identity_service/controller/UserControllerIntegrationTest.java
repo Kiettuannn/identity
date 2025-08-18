8292bdccb9dad2e3b31e1e78824636d66f6af440
KietTuan <theonlytruth25012005@gmail.com>
2025-08-18 16:33:37 +0700

    integration test with testcontainder, I don't understand anything

--- src/test/java/com/kiettuan/identity_service/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/kiettuan/identity_service/controller/UserControllerIntegrationTest.java
@@ -14,25 +14,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDate;
 
 @Slf4j
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource("/test.properties")
+@Testcontainers
 public class UserControllerIntegrationTest {
+    @Container
+    static final MySQLContainer<?> MYSQL_CONTAINER = new MySQLContainer<>("mysql:8.0.43");
+
+    @DynamicPropertySource
+    static void configureDatasource(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MYSQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
+        registry.add("spring.datasource.driverClassName", () -> "com.mysql.cj.jdbc.Driver");
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+    }
 
     @Autowired
     private MockMvc mockMvc;
-
-    @MockitoBean
-    private UserService userService;
 
     private UserCreationRequest request;
     private UserResponse userResponse;
@@ -49,13 +62,6 @@ public class UserControllerIntegrationTest {
                 .dob(dob)
                 .build();
 
-        userResponse = UserResponse.builder()
-                .id("hahahaha")
-                .username("Kiettuan")
-                .firstname("Kiet")
-                .lastname("Tuan")
-                .dob(dob)
-                .build();
     }
 
     // Test with correct data
@@ -67,39 +73,21 @@ public class UserControllerIntegrationTest {
 
         String content = objectmapper.writeValueAsString(request); // convert object to string
 
-        // Mock method call service filter
-        Mockito.when(userService.createUser(ArgumentMatchers.any()))
-                .thenReturn(userResponse);
 
         // WHEN, THEN
-        mockMvc.perform(MockMvcRequestBuilders
-                    .post("/users") // method
-                    .contentType(MediaType.APPLICATION_JSON_VALUE) // data type
-                    .content(content)) // response
-                .andExpect(MockMvcResultMatchers.status().isOk()) // status
-                .andExpect(MockMvcResultMatchers.jsonPath("code").value(1000)) // code
-                .andExpect(MockMvcResultMatchers.jsonPath("result.id").value("hahahaha"));// expect with id
-    }
-
-
-    // Test case: username invalid
-    @Test
-    void createUser_usernameInvalid_fail() throws Exception {
-        // GIVEN
-        request.setUsername("Ke");
-        ObjectMapper objectmapper = new ObjectMapper();
-        objectmapper.registerModule(new JavaTimeModule()); // register for `jackson` package identify LocalDate type
-
-        String content = objectmapper.writeValueAsString(request); // convert object to string
-
-        // WHEN, THEN
-        mockMvc.perform(MockMvcRequestBuilders
+        var response = mockMvc.perform(MockMvcRequestBuilders
                         .post("/users") // method
                         .contentType(MediaType.APPLICATION_JSON_VALUE) // data type
                         .content(content)) // response
-                .andExpect(MockMvcResultMatchers.status().isBadRequest()) // status
-                .andExpect(MockMvcResultMatchers.jsonPath("code").value(1003)) // code
-                .andExpect(MockMvcResultMatchers.jsonPath("message").value("Username must be at least 3 characters"));// expect message
+                .andExpect(MockMvcResultMatchers.status().isOk()) // status
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value(1000)) // code
+                .andExpect(MockMvcResultMatchers.jsonPath("result.username").value("Kiettuan"))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.firstname").value("Kiet"))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.lastname").value("Tuan"));
+
+        log.info("Result: {}",response.andReturn().getResponse().getContentAsString());
+
     }
+
 
 }
